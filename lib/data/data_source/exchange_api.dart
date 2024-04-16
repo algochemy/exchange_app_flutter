30471236894210470898ef5690c66113fbd87a11
David Kim <mycodingtool9@gmail.com>
2024-04-16 11:33:20 +0900

    - feat: exchange 폴더 생성

--- lib/data/data_source/exchange_api.dart
+++ lib/data/data_source/exchange_api.dart
@@ -2,6 +2,13 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ExchangeApi {
+  final http.Client _client;
   final String _baseUrl = 'https://v6.exchangerate-api.com/v6/latest/USD';
+
+  ExchangeApi({http.Client? client}) : _client = client ?? http.Client();
+
+  Future<>
+
+
 
 }
---
+++ lib/presentation/exchange_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ExchangeScreen extends StatelessWidget {
+  const ExchangeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
